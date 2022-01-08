5ef4c86d7f544cb72d7c7e4560b66b48b14d2c4e
zhongweisheng1 <zhongweisheng1@gmail.com>
2022-01-08 18:23:11 +0800

    Added Icon

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
+        Form.setWindowIcon(QtGui.QIcon("./icon.ico"))
         Form.resize(800, 500)
         Form.setMinimumSize(QtCore.QSize(800, 500))
         Form.setMaximumSize(QtCore.QSize(800, 500))
